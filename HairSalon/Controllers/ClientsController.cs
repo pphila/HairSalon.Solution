914fc24654aefcc7c6f86d027d4be36b17b2dfb1
pphila <efl2ice89@gmail.com>
2022-12-24 22:40:03 -0800

    add route and cshtml for create and details page

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,7 +21,26 @@ namespace HairSalon.Controllers
       return View(model);
     }
 
+    public ActionResult Create()
+    {
+      return View();
+    }
 
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    
 
   }
 }
