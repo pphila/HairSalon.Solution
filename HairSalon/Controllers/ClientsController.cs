59066f70f9cdace1c3fc4cf3e379866a34ba807b
pphila <efl2ice89@gmail.com>
2022-12-24 23:13:59 -0800

    add edit and delete pages for clients and routes respectfully

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -40,7 +40,19 @@ namespace HairSalon.Controllers
       return View(thisClient);
     }
 
-    
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }
